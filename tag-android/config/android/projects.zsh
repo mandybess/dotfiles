4293dbb6e823abf8fd451df749e65fbe4b9a0249
Amanda Hill <amandabesshill@gmail.com>
2017-05-18 11:47:22 -0700

    Add separate file for project related aliases and functions
    Keeping them all in one place means it will be easy to 🔎 and ✂️  them
    laterz

---
+++ tag-android/config/android/projects.zsh
@@ -0,0 +1,6 @@
+# all things related to specific projects
+# this will make it easy to 🔎 and ✂️  them later
+
+# upload apk to any littleBits slack channel
+alias ulb="upload -t $LITTLE_BITS_SLACK_BOT_TOKEN"
+
