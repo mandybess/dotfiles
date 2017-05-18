d314541fa5623df06c96136e980aab2064cfcabc
Amanda Hill <amandabesshill@gmail.com>
2017-05-18 11:25:01 -0700

    Add an optional argument for slack bot token
    The default token is the thoughtbot slack token. For other slack teams
    I will have different tokens, so to make my life easier, token is now
    an argument the function accepts 💪🏼

--- tag-android/config/android/amandroid.sh
+++ tag-android/config/android/amandroid.sh
@@ -9,23 +9,26 @@
 
 # constants
 DOMAIN="https://slack.com/api/files.upload"
-TOKEN=$AMANDROID_SLACK_BOT_TOKEN
 
 # defaults
 DEFAULT_CHANNEL="#slackbot-testing"
 # this assumes you are in the project directory and the main app module is 'app'
 DEFAULT_RELATIVE_PATH="$PWD/app/build/outputs/apk/app-debug.apk"
+# this is for thoughtbot slack
+DEFAULT_TOKEN=$AMANDROID_SLACK_BOT_TOKEN
 
 # variables
 CHANNEL=$DEFAULT_CHANNEL
 FILE_NAME=""
 FILE_PATH=$DEFAULT_RELATIVE_PATH
+TOKEN=$DEFAULT_TOKEN
 
-while getopts "c:n:p:" opt ; do
+while getopts "c:n:p:t:" opt ; do
     case "$opt" in
         c) CHANNEL=$OPTARG;;
         n) FILE_NAME=$OPTARG;;
         p) FILE_PATH=$OPTARG;;
+        t) TOKEN=$OPTARG;;
     esac
 done
 
