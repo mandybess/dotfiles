c4f1df1f3b57326a021b55e0f0a1a27e2eebfd02
Amanda Hill <amandabesshill@gmail.com>
2018-04-12 16:49:33 -0700

    Remove Unused Function && Adjust Spacing

--- tag-zsh/config/zsh/completion.zsh
+++ tag-zsh/config/zsh/completion.zsh
@@ -8,17 +8,6 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
 
-# https://coderwall.com/p/jpj_6q
-# Search through history for previous commands matching everything up to current
-# cursor position. Move the cursor to the end of line after each match.
-# thank you Gabe Berke-Williams for this beauty
-autoload -U up-line-or-beginning-search
-autoload -U down-line-or-beginning-search
-zle -N up-line-or-beginning-search
-zle -N down-line-or-beginning-search
-bindkey "^[[A" up-line-or-beginning-search # Up
-bindkey "^[[B" down-line-or-beginning-search # Down
-
 # Colorful lists of possible autocompletions for `ls`
 # zstyle doesn't understand the BSD-style $LSCOLORS at all, so use Linux-style
 # $LS_COLORS
